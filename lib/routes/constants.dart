c4d435ef51ab136c4bd5c47bfde8dc71549e782b
muzair-khan <dev.uzairkhan1486@gmail.com>
2024-02-14 05:39:08 -0600

    go router examples

---
+++ lib/routes/constants.dart
@@ -0,0 +1,10 @@
+class MyAppRoutes{
+  MyAppRoutes._();
+
+    static const myAppRoute = '/';
+    static const homeScreenRoute = '/HOME_SCREEN_ROUTE';
+    static const thirdScreenRoute = '/THIRD_SCREEN_ROUTE';
+
+}
+
+
---
+++ lib/routes/go_router.dart
@@ -0,0 +1,39 @@
+import 'package:go_router/go_router.dart';
+import 'package:learn_flutter/routes/constants.dart';
+import '../main.dart';
+
+
+class MyGoRouter{
+
+  MyGoRouter._();
+
+   static final GoRouter router = GoRouter(
+     // errorBuilder: (context, state) => const ErrorScreen(),
+     //
+
+     // redirect: (context, error) {
+     //  if(1 == 1){
+     //    return MyAppRoutes.homeScreenRoute;
+     //  }
+     //  else{
+     //    return MyAppRoutes.myAppRoute;
+     //  }
+     // },
+
+      routes: [
+        GoRoute(
+            name:MyAppRoutes.myAppRoute,
+            path: "/",builder: (context,state) => const MyApp()),
+        GoRoute(
+            name:MyAppRoutes.homeScreenRoute,
+            path: "${MyAppRoutes.homeScreenRoute}/:data",
+            builder: (context,state) =>  HomeScreen(name: state.pathParameters['data'])),
+        GoRoute(
+            name: MyAppRoutes.thirdScreenRoute,
+            path: MyAppRoutes.thirdScreenRoute,builder: (context,state) => const ThirdScreen()),
+      ]
+  );
+}
+
+
+
