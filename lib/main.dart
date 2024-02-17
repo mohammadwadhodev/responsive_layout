84817f31a9f65b592c79bb7d20f15a8ecefe6bd9
muzair-khan <dev.uzairkhan1486@gmail.com>
2024-02-17 04:50:37 -0600

    navigation back

--- lib/main.dart
+++ lib/main.dart
@@ -12,21 +12,60 @@ void main() {
       home: const MyApp()));
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Basic class"),),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text("PASSING DATA",style: TextStyle(color: Colors.white)),
+      ),
+
+      body: Center(child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            const Text("HOME SCREEN",style: TextStyle(fontSize: 22)),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: () =>{
+              Navigator.push(context, MaterialPageRoute(builder: (context) => SecondScreen("Hello This is Mohammad creations")))
+              
+            }, child: const Text("GO TO NEXT SCREEN"))
+
+          ])),
     );
   }
-
 }
+
+
+class SecondScreen extends StatelessWidget {
+  String data;
+  SecondScreen(this.data, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.blue,
+        title: const Text("PASSING DATA",style: TextStyle(color: Colors.white)),
+      ),
+
+      body: Center(child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            Text(data,style: const TextStyle(fontSize: 22)),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: () =>{}, child: const Text("GO BACK"))
+
+          ])),
+    );
+  }
+}
+
+
 
 
