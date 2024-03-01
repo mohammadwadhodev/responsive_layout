496cfcebe56939327de327349aa79c3becc55809
muzair-khan <dev.uzairkhan1486@gmail.com>
2024-03-01 12:09:33 -0600

    Neo Resume

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,81 @@ import 'package:flutter/material.dart';
 
 
 void main() {
-  runApp(MaterialApp(
-      scrollBehavior: MaterialScrollBehavior().copyWith(
-        dragDevices: {PointerDeviceKind.mouse, PointerDeviceKind.touch, PointerDeviceKind.stylus, PointerDeviceKind.unknown},
-      ),
+  runApp( const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const MyApp()));
+      home: FirstScreen()));
 }
 
-class MyApp extends StatelessWidget {
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.blue,
+        title: const Text("Navigation's",style: TextStyle(color: Colors.white)),
+      ),
+
+      body: Center(child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            const Text("First Screen",style: TextStyle(fontSize: 22)),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) { return const SecondScreen(); }));
+
+            }, child: const Text("NEXT"))
+
+          ])),
+    );
+  }
+}
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.blue,
+        title: const Text("Navigation's",style: TextStyle(color: Colors.white)),
+      ),
+
+      body: Center(child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            const Text("Second Screen",style: TextStyle(fontSize: 22)),
+            const SizedBox(height: 20),
+
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: () {
+              Navigator.of(context).pop();
+
+            }, child: const Text("GO BACK"))
+
+          ])),
+    );
+  }
+}
+
+
+
+
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String data = '';
 
   @override
   Widget build(BuildContext context) {
@@ -27,22 +92,25 @@ class MyApp extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
 
-            const Text("HOME SCREEN",style: TextStyle(fontSize: 22)),
+            Text(data,style: const TextStyle(fontSize: 22)),
             const SizedBox(height: 20),
-            ElevatedButton(onPressed: () =>{
-              Navigator.push(context, MaterialPageRoute(builder: (context) => SecondScreen("Hello This is Mohammad creations")))
-              
+            ElevatedButton(onPressed: () async =>{
+              data =  await Navigator.push(context, MaterialPageRoute(builder: (context) => SecondScreens("Hello This is Mohammad creations"))),
+              setState(() =>{}),
+
             }, child: const Text("GO TO NEXT SCREEN"))
 
           ])),
     );
   }
+
 }
 
 
-class SecondScreen extends StatelessWidget {
+
+class SecondScreens extends StatelessWidget {
   String data;
-  SecondScreen(this.data, {super.key});
+  SecondScreens(this.data, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -59,7 +127,9 @@ class SecondScreen extends StatelessWidget {
 
             Text(data,style: const TextStyle(fontSize: 22)),
             const SizedBox(height: 20),
-            ElevatedButton(onPressed: () =>{}, child: const Text("GO BACK"))
+            ElevatedButton(onPressed: () =>{
+              Navigator.pop(context,"Hello this is me"),
+            }, child: const Text("GO BACK"))
 
           ])),
     );
